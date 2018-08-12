0dd2c96caada9721b65c0aaee3787d21b9ec59f4
Nikita Dozhdev <Dozhdev@phys.vsu.ru>
2018-08-12 20:25:39 +0300

    initial commit

---
+++ src/lol1.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+    val minInt : Int = Int.MIN_VALUE
+    val maxInt : Int = Int.MAX_VALUE
+    val minLong : Long = Long.MIN_VALUE
+    val maxLong : Long = Long.MAX_VALUE
+    val minShort : Short = Short.MIN_VALUE
+    val maxShort : Short = Short.MAX_VALUE
+    val minFloat : Float = Float.MIN_VALUE
+    val maxFloat : Float = Float.MAX_VALUE
+    val minDouble : Double = Double.MIN_VALUE
+    val maxDouble : Double = Double.MAX_VALUE
+    val minByte : Byte = Byte.MIN_VALUE
+    val maxByte : Byte = Byte.MAX_VALUE
+
+
+    println("minInt: "+minInt+", maxInt: "+maxInt)
+    println("minLong: "+minLong+", maxLong: "+maxLong)
+    println("minShort: "+minShort+", maxShort: "+maxShort)
+    println("minFloat: "+minFloat+", maxFloat: "+maxFloat)
+    println("minDouble: "+minDouble+", maxDouble: "+maxDouble)
+    println("minByte: "+minByte+", maxByte: "+maxByte)
+}
