91c67cd69505987351ccd40016a37b1cac7f86ac
Nikita Dozhdev <Dozhdev@phys.vsu.ru>
2018-08-13 20:51:40 +0300

    third commit

--- src/lol1.kt
+++ src/lol1.kt
@@ -1,8 +1,8 @@
 fun main(args: Array<String>) {
-    println("minInt: "+Int.MIN_VALUE+", maxInt: "+Int.MAX_VALUE)
-    println("minLong: "+Long.MIN_VALUE+", maxLong: "+Long.MAX_VALUE)
-    println("minShort: "+Short.MIN_VALUE+", maxShort: "+Short.MAX_VALUE)
-    println("minFloat: "+Float.MIN_VALUE+", maxFloat: "+Float.MAX_VALUE)
-    println("minDouble: "+Double.MIN_VALUE+", maxDouble: "+Double.MAX_VALUE)
-    println("minByte: "+Byte.MIN_VALUE+", maxByte: "+Byte.MAX_VALUE)
+    println("minInt: ${Int.MIN_VALUE}"+" , maxInt: ${Int.MAX_VALUE}")
+    println("minLong: ${Long.MIN_VALUE}"+" , maxLong: ${Long.MAX_VALUE}")
+    println("minShort: ${Short.MIN_VALUE}"+" , maxShort: ${Short.MAX_VALUE}")
+    println("minFloat: ${Float.MIN_VALUE}"+" , maxFloat: ${Float.MAX_VALUE}")
+    println("minDouble: ${Double.MIN_VALUE}"+" , maxDouble: ${Double.MAX_VALUE}")
+    println("minByte: ${Byte.MIN_VALUE}"+" , maxByte: ${Byte.MAX_VALUE}")
 }
