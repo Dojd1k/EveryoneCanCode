afd0a68e2f8fbaa1d4b4fdfe89d779e18f3100cd
Nikita Dozhdev <Dozhdev@phys.vsu.ru>
2018-08-13 14:54:39 +0300

    first commit (task 1.2)

--- src/lol2.kt
+++ src/lol2.kt
@@ -1,3 +1,12 @@
 fun main(args : Array<String>){
-    println("lol")
+    val I = 10
+    val F = 10.2F
+    val D = 10.3
+    val I_sum : Int = I + F.toInt() + D.toInt()
+    val F_sum : Float = I.toFloat() + F + D.toFloat()
+    val D_sum : Double = I.toDouble() + F.toDouble() + D
+    println("I = "+I+", F = "+F+", D = "+ D)
+    println("Sum to Int = "+I_sum)
+    println("Sum to Float = "+F_sum)
+    println("Sum to Double = "+D_sum)
 }
